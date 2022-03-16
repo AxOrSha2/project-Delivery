2d38952702dc934cc5eafc0bad938a370d688eff
JOANMMD <joanmanuelmelodelgado@gmail.com>
2022-03-15 23:50:26 -0500

    add pay component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { HomeComponent } from './components/home/home.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { RegisterComponent } from './components/register/register.component';
 import { StoreComponent } from './components/store/store.component';
+import { PayComponent } from './components/pay/pay.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'admin', component: HomeComponent},
   {path: 'forgot-password', component: ForgotPasswordComponent},
   {path: 'register', component: RegisterComponent},
-  {path: '', component: StoreComponent}
+  {path: '', component: StoreComponent},
+  {path: 'pay', component: PayComponent}
 ];
 
 @NgModule({
