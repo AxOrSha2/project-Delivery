8fdbaaa6350b5b40e2a06181f4da628d8b12a5f3
AxOrSha2 <90012640+AxOrSha2@users.noreply.github.com>
2022-03-28 15:31:23 -0500

    See the total of purchase, can delete the cart.

--- src/app/components/pay/pay.component.ts
+++ src/app/components/pay/pay.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-pay',
@@ -8,16 +10,44 @@ import { Component, OnInit } from '@angular/core';
 export class PayComponent implements OnInit {
 
   shoppingCartList: any = [];
+  totalShopping: number = 0;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.getUserCart();
   }
 
   getUserCart() {
-    //
     this.shoppingCartList = JSON.parse(localStorage.getItem("shoppingCartItems") || '[]');
+    this.calculateTotal()
+  }
+
+  calculateTotal() {
+    this.shoppingCartList.forEach((element: { price: number; }) => {
+      this.totalShopping += element.price;
+    });
+  }
+
+  cleanShoppingCart() {
+    Swal.fire({
+      title: '¿Seguro de eliminar el carrito?',
+      text: "Esta accion no se podra deshacer",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '¡Eliminar!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Carrito eliminado, será redirigido al inicio'
+        })
+        localStorage.removeItem("shoppingCartItems");
+        this.router.navigate(['/']);
+      }
+    })
   }
 
 }
