805b4b2a3bc8a8b76e37f46214f86c456d94ccbc
AxOrSha2 <90012640+AxOrSha2@users.noreply.github.com>
2022-04-01 19:00:18 -0500

    Fixed router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,17 +10,6 @@ import { PayComponent } from './components/pay/pay.component';
 import { RegisterProductsComponent } from './components/register-products/register-products.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'admin', component: HomeComponent},
-  {path: 'forgot-password', component: ForgotPasswordComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'create-product', component: RegisterProductsComponent},
-  {path: 'alter-product/:id', component: RegisterProductsComponent},
-  {path: '', component: StoreComponent},
-  {path: 'pay', component: PayComponent},
-  {path: 'landing', component: LandingPageComponent}
-  
 import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
@@ -56,6 +45,10 @@ const routes: Routes = [
   {
     path: 'pay', 
     component: PayComponent
+  },
+  {
+    path: 'landing', 
+    component: LandingPageComponent
   }
 ];
 
